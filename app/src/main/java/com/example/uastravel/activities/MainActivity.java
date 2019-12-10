c37f0bd035f7a2398ab9fce6af9de6530785a580
fangkiyudho <fangkiyudho@gmail.com>
2019-12-10 23:18:40 +0700

    Splash Screen

--- app/src/main/java/com/example/uastravel/activities/MainActivity.java
+++ app/src/main/java/com/example/uastravel/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.uastravel;
+package com.example.uastravel.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.uastravel.R;
 
 public class MainActivity extends AppCompatActivity {
 
