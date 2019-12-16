e7e1b1769bc598f26acffb40b764c2ae8dcb0451
fangkiyudho <fangkiyudho@gmail.com>
2019-12-16 20:16:58 +0700

    Menu Hotel layout

--- app/src/main/java/com/example/uastravel/fragments/PaketWisata.java
+++ app/src/main/java/com/example/uastravel/fragments/PaketWisata.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 import com.example.uastravel.R;
 
